755812e1646abd8961a1f9e6ed58c16829c781fd
Gautam Arora <gautamsarora3@gmail.com>
2016-02-09 00:16:07 -0500

    Update to be runnable as standalone microapp

--- app/index.js
+++ app/index.js
@@ -2,10 +2,13 @@ var core = require('@gautamarora/core');
 var header = require('@gautamarora/header');
 var self = require('./self');
 
+exports.initBrowserify = core.initBrowserify;
+exports.initBrowserifyOptions = core.initBrowserifyBundleOpts;
+
 module.exports.init = function(app, express) {
   console.log('post app init');
   core.initSass(__dirname);
-  core.init(app);
-  header.init(app);
+  core.init(app, express);
+  header.init(app, express);
   self.init(app, express);
 };
